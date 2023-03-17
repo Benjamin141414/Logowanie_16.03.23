dac969d2b56b200dc6af108bdd58e95315dae476
Bartek Fałek <bartek.falek@ZS1MM.LOCAL>
2023-03-17 15:35:14 +0100

    a

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ async function getUser(){
     const login = document.getElementById("login").value
     const password = document.getElementById("password").value
 
-
     const data = await fetch(`http://localhost:3000/logowanie/${login}/${password}`)
     const json = await data.json()
     console.log(json)
+
+    if(json.user != undefined){
+        localStorage.setItem("upr", JSON.stringify(json))
+    }
+    else{
+        localStorage.setItem("upr", "false")
+    }
 }
