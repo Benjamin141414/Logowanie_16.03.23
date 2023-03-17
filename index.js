fe5c3e17afb65dc3555d8e3cab9f1325d9da754f
Benjamin141414 <123266381+Benjamin141414@users.noreply.github.com>
2023-03-17 14:44:07 +0100

    a

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const users = [
 app.get("/logowanie/:uzytkownik/:password", (req, res)=>{      
     const uzytkownik = req.params.uzytkownik
     const password = req.params.password
+
+    for(let i=0; i<=users.lenght-1; i++){
+        if(users[i].user==user && users[i].pass == pass){
+            res.send({user: user, uprawnienia: users[i].uprawnienia})
+        }
+    }
+        res.json({satus: "niezalogowano"})
 })
 
 app.listen(3000, ()=>{
